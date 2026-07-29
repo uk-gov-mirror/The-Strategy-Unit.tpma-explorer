5c88011868cbfb0ac2edb780f8da6c3f7e3597b3
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2026-07-29 18:28:16 +0100

    Add www and shinycssloaders options

--- R/run_app.R
+++ R/run_app.R
@@ -3,6 +3,17 @@
 run_app <- function() {
   download_all_data()
 
+  golem::add_resource_path(
+    "www",
+    app_sys("app/www")
+  )
+
+  options(
+    # shinycssloaders options
+    spinner.type = 1,
+    spinner.color = "#f9bd07" # su-yellow
+  )
+
   shiny::shinyApp(
     ui = app_ui,
     server = app_server,
