088016a7e21fe1161f4c33457300517a4db1c365
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2026-07-31 14:21:58 +0100

    Use azkit to get container

--- R/utils_server.R
+++ R/utils_server.R
@@ -1,35 +1,3 @@
-#' Get Azure Container
-#' @param ep_uri Character. Endpoint URI.
-#' @param container_name Character. The name of the blob/storage container that
-#'     hosts files you want to read.
-#' @return A blob_container/storage_container list.
-#' @export
-get_container <- function(
-  ep_uri = Sys.getenv("AZ_STORAGE_EP"),
-  container_name = Sys.getenv("AZ_CONTAINER_INPUTS")
-) {
-  # if the app_id variable is empty, we assume that this is running on an Azure
-  # VM, and then we will use Managed Identities for authentication.
-
-  token <- tryCatch(
-    {
-      AzureAuth::get_managed_token("https://storage.azure.com/")
-    },
-    error = function(...) {
-      AzureAuth::get_azure_token(
-        resource = "https://storage.azure.com",
-        tenant = "common",
-        app = "04b07795-8ddb-461a-bbee-02f9e1bf7b46",
-        use_cache = TRUE # avoid browser-authorisation prompt
-      )
-    }
-  )
-  ep_uri |>
-    AzureStor::blob_endpoint(token = token) |>
-    AzureStor::storage_container(container_name)
-}
-
-
 #' Download Inputs Datasets for Specific Geography
 #' @param geography_folder Character. The geography level of the data to
 #'     download. Either "provider" or "lad23cd".
@@ -65,7 +33,9 @@ download_geo_data <- function(
 ) {
   container_dir <- file.path(data_version, geography_folder)
 
-  inputs_container <- get_container()
+  inputs_container <- azkit::get_container(
+    container_name = Sys.getenv("AZ_CONTAINER_INPUTS")
+  )
 
   c(
     "age_sex",
